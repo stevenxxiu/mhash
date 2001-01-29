38cedc062d158055ff0901d792cac8bd646bfaf0
nmav <nmav>
2001-01-29 09:46:29 +0000

    removed 64bit version

--- mhash/lib/mhash_tiger.h
+++ mhash/lib/mhash_tiger.h
@@ -3,27 +3,6 @@
 
 #include "libdefs.h"
 
-#if SIZEOF_UNSIGNED_LONG_INT == 8 /* 64 bit */
-
-#define h0init 0x0123456789ABCDEFLL;
-#define h1init 0xFEDCBA9876543210LL;
-#define h2init 0xF096A5B4C3B2E187LL;
-
-#define TIGER_DATALEN 16
-#define TIGER_DIGESTLEN 3
-#define TIGER_DIGESTSIZE 24
-#define TIGER160_DIGESTSIZE 20
-#define TIGER128_DIGESTSIZE 16
-#define TIGER_DATASIZE 64
-
-typedef struct tiger_ctx {
-  word64 digest[TIGER_DIGESTLEN];  /* Message digest */ 
-  word32 count_l, count_h;		/* 64-bit block count */
-  word8 block[TIGER_DATASIZE];     /* TIGER data buffer */  
-  int index;                       /* index into buffer */
-} TIGER_CTX;
-
-#else /* 32 bit */
 #define h0init 0x89ABCDEF;
 #define h1init 0x01234567;
 #define h2init 0x76543210;
@@ -33,6 +12,8 @@ typedef struct tiger_ctx {
 
 #define TIGER_DATALEN 16
 #define TIGER_DIGESTLEN 6
+#define TIGER128_DIGESTLEN 4
+#define TIGER160_DIGESTLEN 5
 #define TIGER_DIGESTSIZE 24
 #define TIGER160_DIGESTSIZE 20
 #define TIGER128_DIGESTSIZE 16
@@ -41,10 +22,10 @@ typedef struct tiger_ctx {
 typedef struct tiger_ctx {
   word32 digest[TIGER_DIGESTLEN];  /* Message digest */ 
   word32 count_l, count_h;	 /* 64-bit block count */
-  word8 block[TIGER_DATASIZE];     /* TIGER data buffer */  
+  word8 block[TIGER_DATASIZE];     /* RIPEMD data buffer */  
   int index;                             /* index into buffer */
 } TIGER_CTX;
-#endif
+
 
 void tiger_digest(struct tiger_ctx *ctx, word8 * s);
 void tiger_digest160(struct tiger_ctx *ctx, word8 * s);
